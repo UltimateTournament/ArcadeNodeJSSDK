63ddcb3a4ad740c8404a5eb2dfe5dc927b00975f
Martin Rauscher <martin@ultimatetournament.io>
2022-06-16 11:36:50 +0200

    export interface

--- index.ts
+++ index.ts
@@ -5,4 +5,4 @@ import { ArcadeServerSDK, arcadeServerSDK } from "./server";
 const UASDK: ArcadeServerSDK = process.env.UA_MOCK_MODE === "MOCK" ? new Mock() : new arcadeServerSDK();
 const getSDK = () => UASDK;
 
-export { getSDK, ActivateSlipResponse, GetServerStatusResponse, ScoreReport }
+export { getSDK, ArcadeServerSDK, ActivateSlipResponse, GetServerStatusResponse, ScoreReport }
